4f5df4b2bbcf78b7dbe6bd8d9ec9e9c73715e20e
oryankibandi <shreckoryan@gmail.com>
2021-06-13 23:12:22 +0300

    added post upload functionality

--- lib/pages/Upload.dart
+++ lib/pages/Upload.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Upload extends StatefulWidget {
   @override
@@ -6,12 +10,92 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
-  @override
-  Widget build(BuildContext context) {
+  File file;
+  final _picker = ImagePicker();
+  Container buidlSplashScreen() {
     return Container(
-      child: Center(
-        child: Text('Upload'),
+      color: Theme.of(context).primaryColor.withOpacity(0.6),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            'assets/images/upload.svg',
+            // height: 200,
+            width: 200,
+          ),
+          Padding(
+              padding: EdgeInsets.only(top: 10.0),
+              child: ElevatedButton(
+                onPressed: () => selectImage(context),
+                child: Text('Upload Image',
+                    style: TextStyle(color: Colors.white, fontSize: 22.0)),
+              ))
+        ],
       ),
     );
+  }
+
+  Future getImageFromCamera() async {
+    Navigator.of(context).pop();
+    final pickedFile = await _picker.getImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        file = File(pickedFile.path);
+      } else {
+        print('No image selected');
+      }
+    });
+  }
+
+  Future getImageFromGallery() async {
+    Navigator.of(context).pop();
+    final pickedFile = await _picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        file = File(pickedFile.path);
+      } else {
+        print('No image selected');
+      }
+    });
+  }
+
+  selectImage(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            backgroundColor: Colors.white,
+            title: Text('create Post'),
+            children: [
+              SimpleDialogOption(
+                child: Text(
+                  'Photo with camera',
+                  style: TextStyle(color: Colors.black, fontSize: 20.0),
+                ),
+                onPressed: () => getImageFromCamera(),
+              ),
+              SimpleDialogOption(
+                child: Text(
+                  'Image from gallery',
+                  style: TextStyle(color: Colors.black, fontSize: 20.0),
+                ),
+                onPressed: () => getImageFromGallery(),
+              ),
+              SimpleDialogOption(
+                child: Text(
+                  'Cancel',
+                  style: TextStyle(color: Colors.black, fontSize: 20.0),
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return file == null ? buidlSplashScreen() : Text('File selected');
   }
 }
